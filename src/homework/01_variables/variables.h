31dc6cd6e84414ab823405726a3f68171550ebe6
Josafat Hernandez <josafat.hernandez@g.austincc.edu>
2022-02-04 20:33:26 -0600

    hw 1 test cases

--- src/homework/01_variables/variables.h
+++ src/homework/01_variables/variables.h
@@ -3,6 +3,10 @@ int add_numbers(int num1, int num2);
 
 
 
+// Find file variables.h, create an int value-function prototype named multiply_numbers
+ // with one int parameter named num1.
+
+int multiply_numbers(int num1);
 
 
 
