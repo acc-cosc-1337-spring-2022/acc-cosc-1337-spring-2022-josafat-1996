1a7ecc91877f978ba6be51b4dfc0f1952768ee2f
Josafat Hernandez <josafat.hernandez@g.austincc.edu>
2022-02-11 23:14:13 -0600

    hw 2 test cases fail

--- src/homework/02_expressions/expressions.cpp
+++ src/homework/02_expressions/expressions.cpp
@@ -1,4 +1,5 @@
-#include "expressions.h"
+#include "../src/homework/02_expressions/expressions.h"
+
 
 //example
 int add_numbers(int num1, int num2)
@@ -10,5 +11,23 @@ int add_numbers(int num1, int num2)
 //function returns product of num1 and num2
 
 
+
+
+//n file hwexpressions.cpp  write function code, multiply tax_rate and meal_amount and return their product.
+
+double multiply_tax_and_amount(double meal_amount)
+{
+	double product_Result_tax = ((meal_amount / 100) * tax_rate);
+	return product_Result_tax;
+}
+
+// In file hwexpressions.cpp, write function code to multiply  meal_amount and tip_rate, return their product.
+
+
+double multiply_tip_and_amount(double meal_amount, double tip_rate)
+{
+	double product_Result_tip = ((meal_amount / 100) * tip_rate);
+	return product_Result_tip;
+}
 
 
--- src/homework/02_expressions/expressions.h
+++ src/homework/02_expressions/expressions.h
@@ -5,4 +5,14 @@ int add_numbers(int num1, int num2);
 
 
 
+const double tax_rate = 6.75;
 
+//Find file hwexpressions.h, create a double value-return function prototype named 
+// get_sales_tax_amount with one double parameter: meal_amount.
+
+double get_sales_tax_amount(double meal_amount);
+
+//Find file hwexpressions.h, create a double value-return function prototype named 
+// get_tip_amount with two double parameters: meal_amount and tip_rate.
+
+double get_tip_amount(double meal_amount, double tip_rate);
